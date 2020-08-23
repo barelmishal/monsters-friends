189efdbde792ba9e276bd26356ddb0b85b7f2a99
Barel Mishal <dreamwork@dreamwork.network>
2020-08-23 20:57:49 +0300

    Updates

---
+++ precache-manifest.1b9546a748163658718adcf409d19fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1113fc5e0d674aec8bff9d7ed8b70064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdabc540792dad449696",
+    "url": "/static/css/main.3d2d06de.chunk.css"
+  },
+  {
+    "revision": "6d2a3a76b2b44dec6b5d",
+    "url": "/static/js/2.cc0b30fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc0b30fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdabc540792dad449696",
+    "url": "/static/js/main.1561a8c3.chunk.js"
+  },
+  {
+    "revision": "549c3783fa7d3f61ad8c",
+    "url": "/static/js/runtime-main.11dbff2d.js"
+  }
+]);
